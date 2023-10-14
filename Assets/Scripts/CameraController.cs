e01a4a8f0e5aee3e3babbe3454bc46280d10236f
Brayden Bouchard <braydenpaul@live.ca>
2023-10-14 13:09:37 -0400

    Create CameraController script,  Edit scene name

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public float offset;
+    public float offsetSmoothing;
+    public Vector3 playerPosition;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        if(player.transform.localScale.x > 0f)
+        {
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        }
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+}
